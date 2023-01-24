4e2a55d6e7ea57a689fdf7319e1a99a0f2aea2b9
James Cogan <james.m.cogan@gmail.com>
2023-01-24 16:33:16 -0500

    react skeleton

---
+++ src/CoverPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import HomePage from './HomePage'
+
+function CoverPage() {
+    return (
+        <div>
+            <button class="enterButton">Bon' Appetite!</button>
+            <HomePage />
+                
+        </div>
+    )
+}
+export default CoverPage
